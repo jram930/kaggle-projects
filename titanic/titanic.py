a270314dd5c115837186d5eb31f302900d4100a5
jram930 <jram930@gmail.com>
2018-06-16 17:10:06 -0400

    Slight modifications to grid search

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.preprocessing import MinMaxScaler
 
 def display_scores(scores):
     print('Scores: ', scores)
@@ -17,14 +17,16 @@ def display_scores(scores):
 
 
 def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.RandomState(seed=42).permutation(len(data))
+    shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
     test_indices = shuffled_indices[:test_set_size]
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
 
 
-def clean_data(data):
+scaler = MinMaxScaler()
+
+def clean_data(data, training=False):
     # Set the index to the passenger ID
     df = data.set_index('PassengerId')
 
@@ -55,9 +57,16 @@ def clean_data(data):
     df['Embarked'] = df['Embarked'].replace('C', 1)
     df['Embarked'] = df['Embarked'].replace('Q', 2)
 
+    if training:
+        scaler.fit(df[['Age', 'SibSp', 'Parch', 'Fare']])
+
+    df[['Age', 'SibSp', 'Parch', 'Fare']] = scaler.transform(df[['Age', 'SibSp', 'Parch', 'Fare']])
+
     return df
 
+
 if __name__ == '__main__':
+
     # Read in the data
     raw = pd.read_csv('train.csv')
     raw_test = pd.read_csv('test.csv')
@@ -66,29 +75,27 @@ if __name__ == '__main__':
     train, cv = split_train_test(raw, 0.2)
 
     # Clean the data
-    clean_all = clean_data(raw)
-    clean_train = clean_data(train)
-    clean_cv = clean_data(cv)
+    clean_train = clean_data(train, True)
+    clean_val = clean_data(cv)
     clean_test = clean_data(raw_test)
 
     # Pull out the labels. This is the Survived column
-    y_all = clean_all['Survived']
     y_train = clean_train['Survived']
-    y_cv = clean_cv['Survived']
+    y_val = clean_val['Survived']
 
     # Pull out the features. This is every column except for Survived
-    X_all = clean_all.drop(['Survived'], axis=1)
     X_train = clean_train.drop(['Survived'], axis=1)
-    X_cv = clean_cv.drop(['Survived'], axis=1)
+    X_val = clean_val.drop(['Survived'], axis=1)
 
     # Data is prepped for training, train the model
     print('Training model...')
-    model = GridSearchCV(RandomForestClassifier(), [{'bootstrap': [True, False], 'n_estimators': [8,10,15,20,50], 'criterion': ['gini', 'entropy'], 'max_depth': [5,10,15,20,None], 'max_features': [4,6,7]}], cv=20, verbose=1, n_jobs=4)
+    model = GridSearchCV(RandomForestClassifier(), [{'bootstrap': [True, False], 'n_estimators': [8,10,15,20,50], 'criterion': ['gini', 'entropy'], 'max_depth': [5,10,15,20,None], 'max_features': [4,6,7]}], cv=5, verbose=1, n_jobs=8)
+    # model = GridSearchCV(SVC(), [{'C': [0.1,0.5,1.0,2.0,5.0], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'degree': [2,3,4,5]}], verbose=1, n_jobs=8, cv=5)
     model.fit(X_train, y_train)
     print('Done training!')
 
-    pred_cv_y = model.predict(X_cv)
-    print('Accuracy: ', accuracy_score(y_cv, pred_cv_y))
+    pred_val_y = model.predict(X_val)
+    print('Accuracy: ', accuracy_score(y_val, pred_val_y))
 
     # Run on test set and output to csv file
     pred_test_y = model.predict(clean_test)
